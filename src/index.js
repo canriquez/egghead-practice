18d21e0ee20ba0e8eb5e16f2e7da88dc931ded7c
Carlos Anriquez <canriquez@gmail.com>
2020-09-08 18:27:33 -0300

    initial basic view layer with input and add todo button

--- src/index.js
+++ src/index.js
@@ -1,75 +1,58 @@
 import { todos, visibilityFilter } from './todoListReducer'
-import { createStore/* , combineReducers  */ } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 
-/* It uses reduce to accumulate the reducers execution as a single return */
-const combineReducers = (reducers) => {
-    return (state = {}, action) => { //It returns a combined  top level reducer function
-        return Object.keys(reducers).reduce(
-            (nextState, key) => {  //nextState, is the accoumulation named variable
-                nextState[key] = reducers[key]( //it creates the nextState.key property iquals
-                    state[key],                 // to the result of the reduce executed
-                    action                      // for the particulat action
-                );
-                return nextState;               //returns the accumulated state for the next iteration (new key)
-            }, {} //Reduce initial empty object (in this case)
-        );
-    };
-};
 
 const todoApp = combineReducers({
     todos,
     visibilityFilter
-})
-
-const store = createStore(todoApp) // We create a store with the reducer
-
-console.log('Initial State:');
-console.log(store.getState());
-console.log('--------------');
-
-console.log('Dispatching ADD_TODO.');
-store.dispatch({
-    type: 'ADD_TODO',
-    id: 0,
-    text: 'Learn Redux'
 });
 
-console.log('Current State:');
-console.log(store.getState());
-console.log('--------------');
+const store = createStore(todoApp)
+let nextTodoId = 0;
 
-console.log('Dispatching ADD_TODO.');
-store.dispatch({
-    type: 'ADD_TODO',
-    id: 1,
-    text: 'Sign Spotify Job Contract '
-});
+class TodoApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <input ref={
+                    node => {
+                        this.input = node
+                    }
+                } type="text" />
+                <button onClick={() => {
+                    store.dispatch({
+                        type: 'ADD_TODO',
+                        text: this.input.value,
+                        id: nextTodoId += 1,
+                    });
+                    this.input.value = '';
+                }} >
+                    Add Todo
+                </button>
+                <ul>
+                    {this.props.todos.map(todo =>
+                        <li key={todo.id}>
+                            {todo.text}
+                        </li>
+                    )}
+                </ul>
+            </div>
+        )
+    }
+}
 
-console.log('Current State:');
-console.log(store.getState());
-console.log('--------------');
 
+const render = () => {
+    ReactDOM.render(
+        <TodoApp
+            todos={store.getState().todos}
+        />,
+        document.getElementById('root')
+    );
 
-console.log('Dispatching TOGGLE_TODO.');
-store.dispatch({
-    type: 'TOGGLE_TODO',
-    id: 0,
-});
+}
 
-console.log('Current State:');
-console.log(store.getState());
-console.log('--------------');
-
-
-console.log('Dispatching SET_VISIBILITY_FILTER');
-store.dispatch({
-    type: 'SET_VISIBILITY_FILTER',
-    filter: 'SHOW_COMPLETED'
-});
-
-console.log('Current State:');
-console.log(store.getState());
-console.log('--------------');
-
+store.subscribe(render);
+render();
---
+++ src/index_old_02.js
@@ -0,0 +1,61 @@
+import { todos, visibilityFilter } from './todoListReducer'
+import { createStore, combineReducers } from 'redux'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const todoApp = combineReducers({
+    todos,
+    visibilityFilter
+})
+
+const store = createStore(todoApp) // We create a store with the reducer
+
+console.log('Initial State:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching ADD_TODO.');
+store.dispatch({
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux'
+});
+
+console.log('Current State:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching ADD_TODO.');
+store.dispatch({
+    type: 'ADD_TODO',
+    id: 1,
+    text: 'Sign Spotify Job Contract '
+});
+
+console.log('Current State:');
+console.log(store.getState());
+console.log('--------------');
+
+
+console.log('Dispatching TOGGLE_TODO.');
+store.dispatch({
+    type: 'TOGGLE_TODO',
+    id: 0,
+});
+
+console.log('Current State:');
+console.log(store.getState());
+console.log('--------------');
+
+
+console.log('Dispatching SET_VISIBILITY_FILTER');
+store.dispatch({
+    type: 'SET_VISIBILITY_FILTER',
+    filter: 'SHOW_COMPLETED'
+});
+
+console.log('Current State:');
+console.log(store.getState());
+console.log('--------------');
+
